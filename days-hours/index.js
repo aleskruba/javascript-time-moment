048ca4f3505bf1b49d130e2ef145df42005c1361
aleskruba <74367962+aleskruba@users.noreply.github.com>
2023-05-06 21:24:31 +0200

    Update index.js

--- days-hours/index.js
+++ days-hours/index.js
@@ -1,58 +1,70 @@
-const now = moment();
-
 const calendar = (containerEl) => {
-  let selectedDate = null;
-  const renderCalendar = () => {
-    const startOfMonth = moment(now).startOf('month');
-    const endOfMonth = moment(now).endOf('month');
+    let selectedDate = null;
+    let currentMonth = moment();
+    
+    const renderCalendar = () => {
+      const startOfMonth = moment(currentMonth).startOf('month');
+      const endOfMonth = moment(currentMonth).endOf('month');
+      const daysInMonth = endOfMonth.date();
+  
+      const monthName = currentMonth.format('MMMM');
+      const year = currentMonth.format('YYYY');
+  
+      const calendarHeader = `
+        <div class="calendar-header">
+          <button id="prev-month">&lt;</button>
+          <div>${monthName} ${year}</div>
+          <button id="next-month">&gt;</button>
+        </div>
+      `;
 
-    const monthName = now.format('MMMM');
-    const year = now.format('YYYY');
-
-    const calendarHeader = `
-      <div class="calendar-header">
-        <button id="prev-month">&lt;</button>
-        <div>${monthName} ${year}</div>
-        <button id="next-month">&gt;</button>
-      </div>
-    `;
-
-    const calendarDays = `
+      const calendarDays = `
       <div class="calendar-days">
-        <div>Sun</div>
-        <div>Mon</div>
-        <div>Tue</div>
-        <div>Wed</div>
-        <div>Thu</div>
-        <div>Fri</div>
-        <div>Sat</div>
+        <div class="calendar-day">Sun</div>
+        <div class="calendar-day">Mon</div>
+        <div class="calendar-day">Tue</div>
+        <div class="calendar-day">Wed</div>
+        <div class="calendar-day">Thu</div>
+        <div class="calendar-day">Fri</div>
+        <div class="calendar-day">Sat</div>
       </div>
     `;
+    
 
     const calendarDates = [];
     let cells = [];
 
     for (let date = moment(startOfMonth); date <= endOfMonth; date.add(1, 'day')) {
-      const day = date.date();
-      const isToday = date.isSame(moment(), 'day');
-      const isSelected = date.isSame(selectedDate, 'day');
-      const isPast = date.isBefore(moment(), 'day');
-
-      cells.push(`
-        <div
-          class="calendar-date${isToday ? ' calendar-date-today' : ''}${isSelected ? ' calendar-date-selected' : ''}${isPast ? ' calendar-date-past' : ''}"
-          data-date="${date.format('YYYY-MM-DD')}"
-        >
-          ${day}
-        </div>
-      `);
-
-      if (date.day() === 6) {
-        calendarDates.push(`<div class="calendar-row">${cells.join('')}</div>`);
-        cells = [];
-      }
+        const day = date.date();
+        const isToday = date.isSame(moment(), 'day');
+        const isSelected = date.isSame(selectedDate, 'day');
+        const isPast = date.isBefore(moment(), 'day');
+    
+        if (date.date() === 1) {
+            // Add empty cells for days before the first day of the month
+            const emptyCells = date.day();
+            for (let i = 0; i < emptyCells; i++) {
+                cells.push(`
+                    <div class="calendar-date calendar-date-empty"></div>
+                `);
+            }
+        }
+    
+        cells.push(`
+            <div
+                class="calendar-date${isToday ? ' calendar-date-today' : ''}${isSelected ? ' calendar-date-selected' : ''}${isPast ? ' calendar-date-past' : ''}"
+                data-date="${date.format('YYYY-MM-DD')}"
+            >
+                ${day}
+            </div>
+        `);
+    
+        if (date.day() === 6 || day === daysInMonth) {
+            calendarDates.push(`<div class="calendar-row">${cells.join('')}</div>`);
+            cells = [];
+        }
     }
-
+    
     const calendarDatesHtml = calendarDates.join('');
 
     const calendarHtml = `
@@ -65,12 +77,14 @@ const calendar = (containerEl) => {
 
     containerEl.innerHTML = calendarHtml;
     containerEl.querySelectorAll('.calendar-date').forEach((dateEl) => {
+      dateEl.addEventListener('click', () => {
+        const date = moment(dateEl.getAttribute('data-date'));
+        if (date.isSameOrAfter(moment(), 'day')) {
+          selectedDate = date;
+          renderCalendar();
 
-        dateEl.addEventListener('click', () => {
-          const date = moment(dateEl.getAttribute('data-date'));
-          if (date.isSameOrAfter(moment(), 'day')) {
-            selectedDate = date;
-            renderCalendar();
+
+          
       
             // Create a new container for the 24-hour schedule
             const scheduleContainer = document.createElement('div');
@@ -154,21 +168,18 @@ const calendar = (containerEl) => {
       
       
 
-    containerEl.querySelector('#prev-month').addEventListener('click', () => {
-      now.subtract(1, 'month');
-      renderCalendar();
-    });
-
-    containerEl.querySelector('#next-month').addEventListener('click', () => {
-      now.add(1, 'month');
-      renderCalendar();
-    });
+      const prevMonthBtn = containerEl.querySelector('#prev-month');
+      prevMonthBtn.addEventListener('click', () => {
+        currentMonth.subtract(1, 'month');
+        renderCalendar();
+      });
+      
+      const nextMonthBtn = containerEl.querySelector('#next-month');
+      nextMonthBtn.addEventListener('click', () => {
+        currentMonth.add(1, 'month');
+        renderCalendar();
+      });
+    };
+  
+    renderCalendar();
   };
-
-  renderCalendar();
-
-  return {
-    getSelectedDate: () => selectedDate,
-  };
-};
-
